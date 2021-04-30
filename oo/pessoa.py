04c9a9213a4ce077acdcaea4e097cdc5c67d561b
Geilson25 <geilsonfidelis@egmail.com>
2021-04-29 22:40:20 -0300

    Criado atributo complexo "filhos"

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,18 @@
 class Pessoa:
-    def __init__(self, nome, idade=35):
+    def __init__(self, *filhos, nome=None, idade=37):
         self.idade = idade
         self.nome = nome
-
+        self.filhos = list(filhos)
     def cumprimentar(self):
         return f'Olá! {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Fidelis')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Geilson'
-    print(p.nome)
-    print(p.idade)
+    geilson = Pessoa(nome='Geilson')
+    iris = Pessoa(geilson, nome='Iris')
+    print(Pessoa.cumprimentar(iris))
+    print(id(iris))
+    print(iris.cumprimentar())
+    print(iris.nome)
+    print(iris.idade)
+    for filho in iris.filhos:
+        print(filho.nome)
